ddcb12dee27f5675570c50e20e1d0e05c2f94521
Ernest-Tobolkin <ernest1047@gmail.com>
2023-02-20 23:24:13 +0200

    Payment model added

---
+++ backend/models/Payment.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+
+const Payment = new mongoose.Schema({
+    Name: {
+        type: String,
+        required: true
+    },
+    Description: {
+        type: String,
+        required: false
+    },
+    Date: {
+        type: Date,
+        required: true,
+        default:new Date(new Date().toLocaleTimeString("he-IL", {timeZone: "Asia/Jerusalem"}))
+    },
+    Amount: {
+        type: Number,
+        required: true
+    }
+  });
+
+  const payment = mongoose.model('Payment', Payment);
