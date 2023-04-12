56657cfb3e05960b88db0047b90970a86eb1559c
Ernest-Tobolkin <ernest1047@gmail.com>
2023-04-12 17:41:17 +0300

    navbar bootstrap first layout

--- frontend/src/components/layout/navbar.tsx
+++ frontend/src/components/layout/navbar.tsx
@@ -1,21 +1,32 @@
 import {
     Link,
-    Outlet
 } from "react-router-dom";
 
 export const Layout = () => {
     return (
-        <>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/data">CRUD</Link>
-                    </li>
-                </ul>
-            </nav>
-        </>
-    )
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <Link to="/" className="navbar-brand">My App</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/data" className="nav-link">CRUD</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/about" className="nav-link">About</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/contact" className="nav-link">Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      );
 }
